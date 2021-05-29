9c5d1f8107fe3c591ae955c7779527cb7f54d2fb
Ray Luna <leon.luna.ray@gmail.com>
2021-05-29 15:22:54 -0700

    sucessfully got all emoji data to the handleClick function.

--- client/src/components/EmojiBtn/index.js
+++ client/src/components/EmojiBtn/index.js
@@ -1,24 +1,40 @@
-// import React, { useState } from 'react';
+import React from 'react';
 // import { useDispatch } from 'react-redux';
 // import { createPost } from '../../actions/emojiPosts';
 import emojis from '../Emojis';
 import './style.css';
 
-const EmojiBtn = () => {
-  const gridEmojis = emojis;
+class EmojiBtn extends React.Component {
+  render() {
+    const gridEmojis = emojis;
 
-  const handleClick = () => {};
+    // Emoji data sucessfully transferred from the emoji file to here to the console from handle click. Will then send this to the backend to save the mood.
 
-  return (
-    <div className='emoji-div' onClick={handleClick}>
-      {gridEmojis.map(emoji => (
-        <div className='emoji-btn'>
-          <h1>{emoji.emoji}</h1>
-          <p>{emoji.name}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+    const handleClick = (name, secondary, type, level, emoji) => {
+      console.log(name, secondary, type, level, emoji);
+    };
+
+    return (
+      <div className='emoji-div'>
+        {gridEmojis.map(emoji => (
+          <div
+            id={emoji.name}
+            className='emoji-btn'
+            onClick={handleClick(
+              emoji.name,
+              emoji.secondary,
+              emoji.type,
+              emoji.level,
+              emoji.emoji
+            )}
+          >
+            <h1>{emoji.emoji}</h1>
+            <p>{emoji.name}</p>
+          </div>
+        ))}
+      </div>
+    );
+  } // render
+} // emojiBtn
 
 export default EmojiBtn;
