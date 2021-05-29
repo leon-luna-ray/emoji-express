2e234f53ec551247b3b26572c6b3292b77688246
Ray Luna <leon.luna.ray@gmail.com>
2021-05-29 15:50:11 -0700

    update to using state on each emoji btn

--- client/src/components/EmojiBtn/index.js
+++ client/src/components/EmojiBtn/index.js
@@ -1,40 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { useDispatch } from 'react-redux';
 // import { createPost } from '../../actions/emojiPosts';
 import emojis from '../Emojis';
 import './style.css';
 
-class EmojiBtn extends React.Component {
-  render() {
-    const gridEmojis = emojis;
+const EmojiBtn = () => {
+  const gridEmojis = emojis;
 
-    // Emoji data sucessfully transferred from the emoji file to here to the console from handle click. Will then send this to the backend to save the mood.
+  const [postData, setPostData] = useState({
+    name: '',
+    secondary: '',
+    type: '',
+    level: '',
+    emoji: '',
+  });
 
-    const handleClick = (name, secondary, type, level, emoji) => {
-      console.log(name, secondary, type, level, emoji);
-    };
+  // Emoji data sucessfully transferred from the emoji file to here to the console from handle click. Will then send this to the backend to save the mood.
 
-    return (
-      <div className='emoji-div'>
-        {gridEmojis.map(emoji => (
-          <div
-            id={emoji.name}
-            className='emoji-btn'
-            onClick={handleClick(
-              emoji.name,
-              emoji.secondary,
-              emoji.type,
-              emoji.level,
-              emoji.emoji
-            )}
-          >
-            <h1>{emoji.emoji}</h1>
-            <p>{emoji.name}</p>
-          </div>
-        ))}
-      </div>
-    );
-  } // render
-} // emojiBtn
+  // this will submit the post to the backend along with the previous ones
+
+  // Need to figure out how to both handle the click to submit to the db and update the state at the same time.
+  const handleEmoji = e => {
+    e.preventDefault();
+    console.log(postData);
+  };
+
+  return (
+    <div className='emoji-div'>
+      {gridEmojis.map(emoji => (
+        <div
+          id={emoji.name}
+          className='emoji-btn'
+          onClick={e =>
+            setPostData({
+              ...postData,
+              name: emoji.name,
+              secondary: emoji.secondary,
+              type: emoji.type,
+              level: emoji.level,
+              emoji: emoji.emoji,
+            })
+          }
+          onClick={handleEmoji}
+        >
+          <h1>{emoji.emoji}</h1>
+          <p>{emoji.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}; // emojiBtn
 
 export default EmojiBtn;
