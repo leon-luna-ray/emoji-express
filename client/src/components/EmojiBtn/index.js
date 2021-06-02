3802ea3f0cb71a27280a7f191baaaa919c8e3fee
Ray Luna <leon.luna.ray@gmail.com>
2021-06-02 00:35:26 -0700

    refactor code into emoji grid

--- client/src/components/EmojiBtn/index.js
+++ client/src/components/EmojiBtn/index.js
@@ -1,57 +1,64 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { createPost } from '../../actions/emojiPosts';
-import emojis from '../Emojis';
-import './style.css';
+import React from 'react';
 
-const EmojiBtn = () => {
-  const gridEmojis = emojis;
-
-  const [postData, setPostData] = useState({
-    name: '',
-    secondary: '',
-    type: '',
-    level: '',
-    emoji: '',
-  });
-
-  const dispatch = useDispatch();
-
-  // Emoji data sucessfully transferred from the emoji file to here to the console from handle click. Will then send this to the backend to save the mood.
-
-  // this will submit the post to the backend along with the previous ones
-
-  // Need to figure out how to both handle the click to submit to the db and update the state at the same time.
-  const handleEmoji = e => {
-    e.preventDefault();
-    console.log(postData);
-    dispatch(createPost(postData));
-  };
-
+// Props will get passed in from the grid component as an array of objects. This will then be deconstructed and used to render the emoji.
+const emojiBtn = ({ emoji }) => {
   return (
-    <div className='emoji-div'>
-      {gridEmojis.map(emoji => (
-        <div
-          id={emoji.name}
-          className='emoji-btn'
-          onClick={e =>
-            setPostData({
-              ...postData,
-              name: emoji.name,
-              secondary: emoji.secondary,
-              type: emoji.type,
-              level: emoji.level,
-              emoji: emoji.emoji,
-            })
-          }
-          onClick={handleEmoji}
-        >
-          <h1>{emoji.emoji}</h1>
-          <p>{emoji.name}</p>
-        </div>
-      ))}
+    <div className='emoji-btn'>
+      <h1>{emoji.emoji}</h1>
     </div>
   );
-}; // emojiBtn
+};
 
-export default EmojiBtn;
+export default emojiBtn;
+
+// import React, { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { createPost } from '../../actions/emojiPosts';
+// import emojis from '../Emojis';
+// import './style.css';
+
+// const EmojiBtn = () => {
+//   const gridEmojis = emojis;
+
+//   const [postData, setPostData] = useState({
+//     name: '',
+//     secondary: '',
+//     type: '',
+//     level: '',
+//     emoji: '',
+//   });
+
+//   const dispatch = useDispatch();
+
+//   // Emoji data sucessfully transferred from the emoji file to here to the console from handle click. Will then send this to the backend to save the mood.
+
+//   // this will submit the post to the backend along with the previous ones
+
+//   // Need to figure out how to both handle the click to submit to the db and update the state at the same time.
+//   const handleEmoji = e => {
+//     e.preventDefault();
+//     console.log(postData);
+//     // setPostData({
+//     //   ...postData,
+//     //   name: emoji.name,
+//     //   secondary: emoji.secondary,
+//     //   type: emoji.type,
+//     //   level: emoji.level,
+//     //   emoji: emoji.emoji,
+//     // });
+//     dispatch(createPost(postData));
+//   };
+
+//   return (
+//     <div className='emoji-div'>
+//       {gridEmojis.map(emoji => (
+//         <div id={emoji.name} className='emoji-btn' onClick={handleEmoji}>
+//           <h1>{emoji.emoji}</h1>
+//           <p>{emoji.name}</p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }; // emojiBtn
+
+// export default EmojiBtn;
--- client/src/components/EmojiGrid/index.js
+++ client/src/components/EmojiGrid/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import EmojiBtn from '../EmojiBtn';
+import emojis from '../Emojis';
 
-class EmojiGrid extends React.Component {
-  render() {
-    return <EmojiBtn />;
-  }
-}
+const EmojiGrid = () => {
+  const gridEmojis = emojis;
+
+  // Render emoji btns by importing them from the emoji file.
+  // Will need to find a way to create a new row every six buttons.
+  return (
+    <div className='emoji-grid'>
+      {gridEmojis.map(emoji => (
+        <EmojiBtn emoji={emoji} />
+      ))}
+    </div>
+  );
+};
 
 export default EmojiGrid;
