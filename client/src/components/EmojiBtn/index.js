237d64188832c2ddf45b2c787c5f13f9cded7b4c
Ray Luna <leon.luna.ray@gmail.com>
2021-06-02 14:21:37 -0700

    successful post on click

--- client/src/components/EmojiBtn/index.js
+++ client/src/components/EmojiBtn/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
+// import redux hook useDispatch
+import { useDispatch } from 'react-redux';
+import { createPost } from '../../actions/emojiPosts';
 
 // Props will get passed in from the grid component as an array of objects. This will then be deconstructed and used to render the emoji.
-const emojiBtn = ({ emoji }) => {
+const EmojiBtn = ({ emoji }) => {
+  const dispatch = useDispatch();
+
+  const handleClick = e => {
+    e.preventDefault();
+    dispatch(
+      createPost({
+        name: emoji.name,
+        secondary: emoji.secondary,
+        type: emoji.type,
+        level: emoji.level,
+        emoji: emoji.emoji,
+      })
+    );
+  };
+
   return (
-    <div id={emoji.name} className='emoji-btn'>
+    <div
+      id={emoji.name}
+      name={emoji.name}
+      className='emoji-btn'
+      onClick={handleClick}
+    >
       <h1 className='emoji'>{emoji.emoji}</h1>
       <p className='emoji-name'>{emoji.name}</p>
     </div>
   );
 };
 
-export default emojiBtn;
+export default EmojiBtn;
--- client/src/components/EmojiGrid/index.js
+++ client/src/components/EmojiGrid/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import EmojiBtn from '../EmojiBtn';
 import emojis from '../Emojis';
 import './style.css';
@@ -6,31 +6,13 @@ import './style.css';
 const EmojiGrid = () => {
   const gridEmojis = emojis;
 
-  const [postData, setPostData] = useState({
-    name: '',
-    secondary: '',
-    type: '',
-    level: '',
-    emoji: '',
-  });
-
-  const handleClick = e => {
-    e.preventDefault();
-    console.log('click!');
-  };
-
   // Render emoji btns by importing them from the emoji file.
   // Will need to find a way to create a new row every six buttons.
   return (
     <div className='emoji-grid'>
       <div className='emoji-div'>
         {gridEmojis.map(emoji => (
-          <EmojiBtn
-            key={emoji.name}
-            emoji={emoji}
-            value={postData.emoji}
-            onClick={e => setPostData({ ...postData, emoji: e.target.value })}
-          />
+          <EmojiBtn key={emoji.name} emoji={emoji} />
         ))}
       </div>
     </div>
