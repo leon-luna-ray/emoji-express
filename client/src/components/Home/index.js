f563aef1c5754b61bcba91be7bbbbf7623ffbb7d
Ray Luna <leon.luna.ray@gmail.com>
2021-06-02 18:45:51 -0700

    update emoji posts

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
 import EmojiGrid from '../EmojiGrid';
+import EmojiPosts from '../EmojiPosts';
 
 const HomePage = () => {
   return (
     <Container>
+      <EmojiPosts />
       <EmojiGrid />
     </Container>
   );
