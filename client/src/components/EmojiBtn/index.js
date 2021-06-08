e7552e4b371a25b3d0d0faec8313433a819a8f56
Ray Luna <leon.luna.ray@gmail.com>
2021-06-07 22:31:03 -0700

    reload on click to display emojis until better soloution found

--- client/src/components/EmojiBtn/index.js
+++ client/src/components/EmojiBtn/index.js
@@ -1,23 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import redux hook useDispatch
 import { useDispatch } from 'react-redux';
 import { createPost } from '../../actions/emojiPosts';
 
 // Props will get passed in from the grid component as an array of objects. This will then be deconstructed and used to render the emoji.
 const EmojiBtn = ({ emoji }) => {
+  const [emojiState] = useState(emoji);
   const dispatch = useDispatch();
 
   const handleClick = e => {
     e.preventDefault();
-    dispatch(
-      createPost({
-        name: emoji.name,
-        secondary: emoji.secondary,
-        type: emoji.type,
-        level: emoji.level,
-        emoji: emoji.emoji,
-      })
-    );
+    dispatch(createPost(emojiState));
+    // Using window reload on click to get emoji on the page on click. Need to find a way to use state to do this since the time since does show up on click without page reload. 🤔
+    window.location.reload(false);
   };
 
   return (
