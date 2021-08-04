29b8888e561925cf495e221a177d3ead1f41e900
Ray Luna <leon.luna.ray@gmail.com>
2021-08-03 19:42:47 -0700

    update emojis

--- client/src/components/Emojis.js
+++ client/src/components/Emojis.js
@@ -1,175 +1,175 @@
 // need to research passing props to render each emoji with props and then have that data passed to the server on click
 
-const emojis = {
-  afraid: {
+const emojis = [
+  {
     name: 'Afraid',
     secondary: null,
     type: 'Afraid',
     level: 1,
     emoji: '😓',
   },
-  angry: {
+  {
     name: 'Angry',
     secondary: null,
     type: 'Angry',
     level: 1,
     emoji: '😡',
   },
-  annoyed: {
+  {
     name: 'Annoyed',
     secondary: 'Frustrated',
     type: 'Angry',
     level: 3,
     emoji: '🙄',
   },
-  anxious: {
+  {
     name: 'Anxious',
     secondary: null,
     type: 'Fearful',
     level: 2,
     emoji: '😣',
   },
-  bored: {
+  {
     name: 'Bored',
     secondary: null,
     type: 'Bad',
     level: 2,
     emoji: '😑',
   },
-  calm: {
+  {
     name: 'Calm',
     secondary: null,
     type: 'Happy',
     level: 2,
     emoji: '😌',
   },
-  confused: {
+  {
     name: 'Confused',
     secondary: null,
     type: 'Surprised',
     level: 2,
     emoji: '🤔',
   },
-  crazy: {
+  {
     name: 'Crazy',
     secondary: 'Excited',
     type: 'Surprised',
     level: 3,
     emoji: '🤪',
   },
-  disgusted: {
+  {
     name: 'Disgusted',
     secondary: null,
     type: 'Disgusted',
     level: 1,
     emoji: '😒',
   },
-  embarrassed: {
+  {
     name: 'Embarrassed',
     secondary: 'Disapproving',
     type: 'Disgusted',
     level: 3,
     emoji: '😅',
   },
-  excited: {
+  {
     name: 'Excited',
     secondary: null,
     type: 'Surprised',
     level: 2,
     emoji: '😆',
   },
-  happy: {
+  {
     name: 'Happy',
     secondary: null,
     type: 'Happy',
     level: 1,
     emoji: '🙂',
   },
-  lonely: {
+  {
     name: 'Lonely',
     secondary: null,
     type: 'Sad',
     level: 2,
     emoji: '😔',
   },
-  nostalgic: {
+  {
     name: 'Nostalgic',
     secondary: 'Peaceful',
     type: 'Happy',
     level: 3,
     emoji: '🥲',
   },
-  numb: {
+  {
     name: 'Numb',
     secondary: 'Distant',
     type: 'Angry',
     level: 3,
     emoji: '😐',
   },
-  optimistic: {
+  {
     name: 'Optimistic',
     secondary: null,
     type: 'Happy',
     level: 2,
     emoji: '😃',
   },
-  proud: {
+  {
     name: 'Proud',
     secondary: null,
     type: 'Happy',
     level: 2,
     emoji: '😎',
   },
-  sad: {
+  {
     name: 'Sad',
     secondary: null,
     type: 'Sad',
     level: 1,
     emoji: '🙁',
   },
-  sick: {
+  {
     name: 'Sick',
     secondary: 'Awful',
     type: 'Disgusted',
     level: 3,
     emoji: '🤢',
   },
-  silly: {
+  {
     name: 'Silly',
     secondary: 'Playful',
     type: 'Happy',
     level: 3,
     emoji: '🤡',
   },
-  sleepy: {
+  {
     name: 'Sleepy',
     secondary: 'Tired',
     type: 'Bad',
     level: 3,
     emoji: '😴',
   },
-  stressed: {
+  {
     name: 'Stressed',
     secondary: null,
     type: 'Bad',
     level: 2,
     emoji: '😤',
   },
-  surprised: {
+  {
     name: 'Surprised',
     secondary: null,
     type: 'Surprised',
     level: 3,
     emoji: '😮',
   },
-  tired: {
+  {
     name: 'Tired',
     secondary: null,
     type: 'Bad',
     level: 2,
     emoji: '🥱',
   },
-}; // emojis
+]; // emojis
 
 export default emojis;
 
