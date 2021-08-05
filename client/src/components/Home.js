5f411303578bfec3397eb0e8dd05f8459192c609
Ray Luna <leon.luna.ray@gmail.com>
2021-08-05 00:27:50 -0700

    change home to functional comp

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,20 +5,18 @@ import { Container } from '@material-ui/core';
 import EmojiGrid from './EmojiGrid';
 // import EmojiPosts from '../EmojiPosts';
 
-class Home extends React.Component {
+const Home = () => {
   // May need to be async as this will need to be sent to the db, test to see if you can get it into the db. Will re-render when the data is returned.
-  newUserEmoji = async (emoji) => {
+  const newUserEmoji = (emoji) => {
     console.log(`log the emoji clicked: ${emoji}`);
   };
 
-  render() {
-    return (
-      <Container>
-        <EmojiGrid newUserEmoji={this.newUserEmoji} />
-        {/* <EmojiPosts /> */}
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <EmojiGrid newUserEmoji={newUserEmoji} />
+      {/* <EmojiPosts /> */}
+    </Container>
+  );
+};
 
 export default Home;
