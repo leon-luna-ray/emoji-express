070deaa5fc20e222c8fc41e1363598547d5e8431
Ray Luna <leon.luna.ray@gmail.com>
2021-08-08 03:46:35 -0700

    setup for rendering rows

--- client/src/components/EmojiGrid.js
+++ client/src/components/EmojiGrid.js
@@ -4,7 +4,7 @@ import emojis from './Emojis';
 import './EmojiGrid.css';
 
 const EmojiGrid = ({ setUserEmoji }) => {
-  const gridEmojis = emojis;
+  const emojiSet = emojis;
 
   // Render emoji btns by importing them from the emoji file.
   // Will need to find a way to create a new row every six buttons.
@@ -13,23 +13,31 @@ const EmojiGrid = ({ setUserEmoji }) => {
   // try using the modulus operator
 
   // create another return for the rendered buttons
-  const renderedBtns = () => {
-    <div className='row'>{emojis.forEach((emoji, index) => {})}</div>;
+  const renderedRows = () => {
+    emojiSet.forEach((row, index) => {
+      const renderedRow = () => {
+        row.map((emoji) => {
+          return (
+            <div className='emoji-div'>
+              <EmojiBtn
+                key={emoji.name}
+                emoji={emoji}
+                setUserEmoji={setUserEmoji}
+              />
+            </div>
+          );
+        });
+      };
+
+      return (
+        <div key={index} className='row'>
+          {renderedRow}
+        </div>
+      );
+    });
   };
 
-  return (
-    <div className='emoji-grid'>
-      <div className='emoji-div'>
-        {gridEmojis.map((emoji) => (
-          <EmojiBtn
-            key={emoji.name}
-            emoji={emoji}
-            setUserEmoji={setUserEmoji}
-          />
-        ))}
-      </div>
-    </div>
-  );
+  return <div className='emoji-gird'>{renderedRows}</div>;
 };
 
 export default EmojiGrid;
--- client/src/components/Emojis.js
+++ client/src/components/Emojis.js
@@ -1,412 +1,182 @@
 // need to research passing props to render each emoji with props and then have that data passed to the server on click
 
 const emojis = [
-  {
-    name: 'Afraid',
-    secondary: null,
-    type: 'Afraid',
-    level: 1,
-    emoji: '😓',
-  },
-  {
-    name: 'Angry',
-    secondary: null,
-    type: 'Angry',
-    level: 1,
-    emoji: '😡',
-  },
-  {
-    name: 'Annoyed',
-    secondary: 'Frustrated',
-    type: 'Angry',
-    level: 3,
-    emoji: '🙄',
-  },
-  {
-    name: 'Anxious',
-    secondary: null,
-    type: 'Fearful',
-    level: 2,
-    emoji: '😣',
-  },
-  {
-    name: 'Bored',
-    secondary: null,
-    type: 'Bad',
-    level: 2,
-    emoji: '😑',
-  },
-  {
-    name: 'Calm',
-    secondary: null,
-    type: 'Happy',
-    level: 2,
-    emoji: '😌',
-  },
-  {
-    name: 'Confused',
-    secondary: null,
-    type: 'Surprised',
-    level: 2,
-    emoji: '🤔',
-  },
-  {
-    name: 'Crazy',
-    secondary: 'Excited',
-    type: 'Surprised',
-    level: 3,
-    emoji: '🤪',
-  },
-  {
-    name: 'Disgusted',
-    secondary: null,
-    type: 'Disgusted',
-    level: 1,
-    emoji: '😒',
-  },
-  {
-    name: 'Embarrassed',
-    secondary: 'Disapproving',
-    type: 'Disgusted',
-    level: 3,
-    emoji: '😅',
-  },
-  {
-    name: 'Excited',
-    secondary: null,
-    type: 'Surprised',
-    level: 2,
-    emoji: '😆',
-  },
-  {
-    name: 'Happy',
-    secondary: null,
-    type: 'Happy',
-    level: 1,
-    emoji: '🙂',
-  },
-  {
-    name: 'Lonely',
-    secondary: null,
-    type: 'Sad',
-    level: 2,
-    emoji: '😔',
-  },
-  {
-    name: 'Nostalgic',
-    secondary: 'Peaceful',
-    type: 'Happy',
-    level: 3,
-    emoji: '🥲',
-  },
-  {
-    name: 'Numb',
-    secondary: 'Distant',
-    type: 'Angry',
-    level: 3,
-    emoji: '😐',
-  },
-  {
-    name: 'Optimistic',
-    secondary: null,
-    type: 'Happy',
-    level: 2,
-    emoji: '😃',
-  },
-  {
-    name: 'Proud',
-    secondary: null,
-    type: 'Happy',
-    level: 2,
-    emoji: '😎',
-  },
-  {
-    name: 'Sad',
-    secondary: null,
-    type: 'Sad',
-    level: 1,
-    emoji: '🙁',
-  },
-  {
-    name: 'Sick',
-    secondary: 'Awful',
-    type: 'Disgusted',
-    level: 3,
-    emoji: '🤢',
-  },
-  {
-    name: 'Silly',
-    secondary: 'Playful',
-    type: 'Happy',
-    level: 3,
-    emoji: '🤡',
-  },
-  {
-    name: 'Sleepy',
-    secondary: 'Tired',
-    type: 'Bad',
-    level: 3,
-    emoji: '😴',
-  },
-  {
-    name: 'Stressed',
-    secondary: null,
-    type: 'Bad',
-    level: 2,
-    emoji: '😤',
-  },
-  {
-    name: 'Surprised',
-    secondary: null,
-    type: 'Surprised',
-    level: 3,
-    emoji: '😮',
-  },
-  {
-    name: 'Tired',
-    secondary: null,
-    type: 'Bad',
-    level: 2,
-    emoji: '🥱',
-  },
+  [
+    {
+      name: 'Afraid',
+      secondary: null,
+      type: 'Afraid',
+      level: 1,
+      emoji: '😓',
+    },
+    {
+      name: 'Angry',
+      secondary: null,
+      type: 'Angry',
+      level: 1,
+      emoji: '😡',
+    },
+    {
+      name: 'Annoyed',
+      secondary: 'Frustrated',
+      type: 'Angry',
+      level: 3,
+      emoji: '🙄',
+    },
+    {
+      name: 'Anxious',
+      secondary: null,
+      type: 'Fearful',
+      level: 2,
+      emoji: '😣',
+    },
+    {
+      name: 'Bored',
+      secondary: null,
+      type: 'Bad',
+      level: 2,
+      emoji: '😑',
+    },
+    {
+      name: 'Calm',
+      secondary: null,
+      type: 'Happy',
+      level: 2,
+      emoji: '😌',
+    },
+  ],
+  [
+    {
+      name: 'Confused',
+      secondary: null,
+      type: 'Surprised',
+      level: 2,
+      emoji: '🤔',
+    },
+    {
+      name: 'Crazy',
+      secondary: 'Excited',
+      type: 'Surprised',
+      level: 3,
+      emoji: '🤪',
+    },
+    {
+      name: 'Disgusted',
+      secondary: null,
+      type: 'Disgusted',
+      level: 1,
+      emoji: '😒',
+    },
+    {
+      name: 'Embarrassed',
+      secondary: 'Disapproving',
+      type: 'Disgusted',
+      level: 3,
+      emoji: '😅',
+    },
+    {
+      name: 'Excited',
+      secondary: null,
+      type: 'Surprised',
+      level: 2,
+      emoji: '😆',
+    },
+    {
+      name: 'Happy',
+      secondary: null,
+      type: 'Happy',
+      level: 1,
+      emoji: '🙂',
+    },
+  ],
+  [
+    {
+      name: 'Lonely',
+      secondary: null,
+      type: 'Sad',
+      level: 2,
+      emoji: '😔',
+    },
+    {
+      name: 'Nostalgic',
+      secondary: 'Peaceful',
+      type: 'Happy',
+      level: 3,
+      emoji: '🥲',
+    },
+    {
+      name: 'Numb',
+      secondary: 'Distant',
+      type: 'Angry',
+      level: 3,
+      emoji: '😐',
+    },
+    {
+      name: 'Optimistic',
+      secondary: null,
+      type: 'Happy',
+      level: 2,
+      emoji: '😃',
+    },
+    {
+      name: 'Proud',
+      secondary: null,
+      type: 'Happy',
+      level: 2,
+      emoji: '😎',
+    },
+    {
+      name: 'Sad',
+      secondary: null,
+      type: 'Sad',
+      level: 1,
+      emoji: '🙁',
+    },
+  ],
+  [
+    {
+      name: 'Sick',
+      secondary: 'Awful',
+      type: 'Disgusted',
+      level: 3,
+      emoji: '🤢',
+    },
+    {
+      name: 'Silly',
+      secondary: 'Playful',
+      type: 'Happy',
+      level: 3,
+      emoji: '🤡',
+    },
+    {
+      name: 'Sleepy',
+      secondary: 'Tired',
+      type: 'Bad',
+      level: 3,
+      emoji: '😴',
+    },
+    {
+      name: 'Stressed',
+      secondary: null,
+      type: 'Bad',
+      level: 2,
+      emoji: '😤',
+    },
+    {
+      name: 'Surprised',
+      secondary: null,
+      type: 'Surprised',
+      level: 3,
+      emoji: '😮',
+    },
+    {
+      name: 'Tired',
+      secondary: null,
+      type: 'Bad',
+      level: 2,
+      emoji: '🥱',
+    },
+  ],
 ]; // emojis
 
 export default emojis;
-
-// emojis object
-
-// const emojis = {
-//   afraid: {
-//     name: 'Afraid',
-//     secondary: null,
-//     type: 'Afraid',
-//     level: 1,
-//     emoji: '😓',
-//   },
-//   angry: {
-//     name: 'Angry',
-//     secondary: null,
-//     type: 'Angry',
-//     level: 1,
-//     emoji: '😡',
-//   },
-//   annoyed: {
-//     name: 'Annoyed',
-//     secondary: 'Frustrated',
-//     type: 'Angry',
-//     level: 3,
-//     emoji: '🙄',
-//   },
-//   anxious: {
-//     name: 'Anxious',
-//     secondary: null,
-//     type: 'Fearful',
-//     level: 2,
-//     emoji: '😣',
-//   },
-//   bored: {
-//     name: 'Bored',
-//     secondary: null,
-//     type: 'Bad',
-//     level: 2,
-//     emoji: '😑',
-//   },
-//   calm: {
-//     name: 'Calm',
-//     secondary: null,
-//     type: 'Happy',
-//     level: 2,
-//     emoji: '😌',
-//   },
-//   confused: {
-//     name: 'Confused',
-//     secondary: null,
-//     type: 'Surprised',
-//     level: 2,
-//     emoji: '🤔',
-//   },
-//   crazy: {
-//     name: 'Crazy',
-//     secondary: 'Excited',
-//     type: 'Surprised',
-//     level: 3,
-//     emoji: '🤪',
-//   },
-//   disgusted: {
-//     name: 'Disgusted',
-//     secondary: null,
-//     type: 'Disgusted',
-//     level: 1,
-//     emoji: '😒',
-//   },
-//   embarrassed: {
-//     name: 'Embarrassed',
-//     secondary: 'Disapproving',
-//     type: 'Disgusted',
-//     level: 3,
-//     emoji: '😅',
-//   },
-//   excited: {
-//     name: 'Excited',
-//     secondary: null,
-//     type: 'Surprised',
-//     level: 2,
-//     emoji: '😆',
-//   },
-//   happy: {
-//     name: 'Happy',
-//     secondary: null,
-//     type: 'Happy',
-//     level: 1,
-//     emoji: '🙂',
-//   },
-//   lonely: {
-//     name: 'Lonely',
-//     secondary: null,
-//     type: 'Sad',
-//     level: 2,
-//     emoji: '😔',
-//   },
-//   nostalgic: {
-//     name: 'Nostalgic',
-//     secondary: 'Peaceful',
-//     type: 'Happy',
-//     level: 3,
-//     emoji: '🥲',
-//   },
-//   numb: {
-//     name: 'Numb',
-//     secondary: 'Distant',
-//     type: 'Angry',
-//     level: 3,
-//     emoji: '😐',
-//   },
-//   optimistic: {
-//     name: 'Optimistic',
-//     secondary: null,
-//     type: 'Happy',
-//     level: 2,
-//     emoji: '😃',
-//   },
-//   proud: {
-//     name: 'Proud',
-//     secondary: null,
-//     type: 'Happy',
-//     level: 2,
-//     emoji: '😎',
-//   },
-//   sad: {
-//     name: 'Sad',
-//     secondary: null,
-//     type: 'Sad',
-//     level: 1,
-//     emoji: '🙁',
-//   },
-//   sick: {
-//     name: 'Sick',
-//     secondary: 'Awful',
-//     type: 'Disgusted',
-//     level: 3,
-//     emoji: '🤢',
-//   },
-//   silly: {
-//     name: 'Silly',
-//     secondary: 'Playful',
-//     type: 'Happy',
-//     level: 3,
-//     emoji: '🤡',
-//   },
-//   sleepy: {
-//     name: 'Sleepy',
-//     secondary: 'Tired',
-//     type: 'Bad',
-//     level: 3,
-//     emoji: '😴',
-//   },
-//   stressed: {
-//     name: 'Stressed',
-//     secondary: null,
-//     type: 'Bad',
-//     level: 2,
-//     emoji: '😤',
-//   },
-//   surprised: {
-//     name: 'Surprised',
-//     secondary: null,
-//     type: 'Surprised',
-//     level: 3,
-//     emoji: '😮',
-//   },
-//   tired: {
-//     name: 'Tired',
-//     secondary: null,
-//     type: 'Bad',
-//     level: 2,
-//     emoji: '🥱',
-//   },
-// }; // emojis object
-
-// previous code
-
-// const emojiDiv = document.getElementById('emoji-btns');
-
-// function emojiBtn(name, emoji) {
-//   return `
-//     <div id="${name}" class="col s2 center-align emoji-btn" value="${emoji}" onclick="addMood(this.id, this.value)">
-//         <h2 class="emoji">${emoji}</h2>
-//         <p class="name-title">${name}</p>
-//     </div>`;
-// }
-
-// function generateBtns() {
-//   const buttons = [];
-
-//   for (let i = 0; i < names.length; i++) {
-//     const button = emojiBtn(names[i].name, names[i].emoji);
-//     buttons.push(button);
-//   }
-
-//   return buttons;
-// }
-
-// function emojiGrid() {
-//   const buttons = generateBtns();
-//   const btnGrid = [];
-
-//   const newRow = `
-//     <div class="row">
-//     `;
-//   const endRow = `
-//     </div> <!-- row -->
-//     `;
-
-//   // Loop to insert rows every 6 btns
-//   for (let i = 0; i < buttons.length; i++) {
-//     if (i === 0) {
-//       btnGrid.push(newRow);
-//       btnGrid.push(buttons[i]);
-//     } else if (i % 6 === 0) {
-//       btnGrid.push(endRow);
-//       btnGrid.push(newRow);
-//       btnGrid.push(buttons[i]);
-//     } else {
-//       btnGrid.push(buttons[i]);
-//     }
-//   }
-
-//   btnGrid.push(endRow);
-
-//   const markup = btnGrid.join(' ');
-//   emojiDiv.innerHTML = markup;
-// } // emojiGrid
-
-// function addMood(name, emoji) {
-//   console.log(name, emoji);
-
-//   // there may be an issue with getting the emoji back from the front end?
-
-//   // see if you can still get the emoji to come back as it seemed to be working before.....  otherwise will need to get it from the names array`
-// }
-
-// // Render buttons on load
-// emojiGrid();
