a043233237a0c6b5b11adf519d9ba9abbfdfea3f
Ray Luna <leon.luna.ray@gmail.com>
2021-08-08 13:39:36 -0700

    successfully created rows

--- client/src/components/EmojiGrid.js
+++ client/src/components/EmojiGrid.js
@@ -1,43 +1,33 @@
 import React from 'react';
-import EmojiBtn from './EmojiBtn';
 import emojis from './Emojis';
 import './EmojiGrid.css';
 
 const EmojiGrid = ({ setUserEmoji }) => {
   const emojiSet = emojis;
 
-  // Render emoji btns by importing them from the emoji file.
-  // Will need to find a way to create a new row every six buttons.
-  // you may want to try doing two returns within here to get the styling
-
-  // try using the modulus operator
-
-  // create another return for the rendered buttons
-  const renderedRows = () => {
-    emojiSet.forEach((row, index) => {
-      const renderedRow = () => {
-        row.map((emoji) => {
-          return (
-            <div className='emoji-div'>
-              <EmojiBtn
-                key={emoji.name}
-                emoji={emoji}
-                setUserEmoji={setUserEmoji}
-              />
-            </div>
-          );
-        });
-      };
-
-      return (
-        <div key={index} className='row'>
-          {renderedRow}
-        </div>
-      );
-    });
+  const renderBtn = (emoji, index) => {
+    return (
+      <div
+        key={index}
+        id={emoji.name}
+        className='emoji-btn col'
+        onClick={() => setUserEmoji(emoji)}
+      >
+        <h1 className='emoji'>{emoji.emoji}</h1>
+        <p className='emoji-name'>{emoji.name}</p>
+      </div>
+    );
   };
 
-  return <div className='emoji-gird'>{renderedRows}</div>;
+  const renderRows = (row, index) => {
+    return (
+      <div key={index} className='row'>
+        {row.map(renderBtn)}
+      </div>
+    );
+  };
+
+  return <div className='emoji-grid grid'>{emojiSet.map(renderRows)}</div>;
 };
 
 export default EmojiGrid;
