2b4873635daa34439372ecd66f7d1dac1741130a
Ray Luna <leon.luna.ray@gmail.com>
2023-05-13 19:35:34 -0700

    upgrade to react 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk';
-// import reducers from './reducers';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-// const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const container = document.getElementById('app');
+const root = createRoot(container);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(<App />);
