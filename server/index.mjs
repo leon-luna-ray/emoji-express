bbe069f8c96f1aa55b29428732bc146d233d7856
Ray Luna <leon.luna.ray@gmail.com>
2023-07-15 13:08:36 -0700

    refator frontend to pull and save to db

--- server/index.mjs
+++ server/index.mjs
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
-import records from "./routes/record.mjs";
+import posts from "./routes/post.mjs";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/record", records);
+app.use("/api/v1/posts", posts);
 
 // start the Express server
 app.listen(PORT, () => {
--- server/routes/post.mjs
+++ server/routes/post.mjs
@@ -4,16 +4,15 @@ import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
-// This section will help you get a list of all the records.
+// GET
 router.get("/", async (req, res) => {
-  let collection = await db.collection("records");
+  console.log('fetch request')
+  let collection = await db.collection("posts");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
 });
-
-// This section will help you get a single record by id
 router.get("/:id", async (req, res) => {
-  let collection = await db.collection("records");
+  let collection = await db.collection("posts");
   let query = {_id: new ObjectId(req.params.id)};
   let result = await collection.findOne(query);
 
@@ -21,19 +20,35 @@ router.get("/:id", async (req, res) => {
   else res.send(result).status(200);
 });
 
-// This section will help you create a new record.
+// POST
 router.post("/", async (req, res) => {
   let newDocument = {
+    _id: req.body._id,
+    createdAt: req.body.createdAt,
     name: req.body.name,
-    position: req.body.position,
+    secondary: req.body.secondary,
+    type: req.body.type,
     level: req.body.level,
+    emoji: req.body.emoji,
   };
-  let collection = await db.collection("records");
-  let result = await collection.insertOne(newDocument);
-  res.send(result).status(204);
+
+  let collection = await db.collection("posts");
+
+  try {
+    let result = await collection.insertOne(newDocument);
+    if(result.acknowledged && result.insertedId) {
+      res.json({acknowledged: true, insertedId: result.insertedId});
+    } else {
+      res.status(500).json({error: "Could not insert document"});
+    }
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({error: "Could not insert document"});
+  }
 });
 
-// This section will help you update a record by id.
+
+// PATCH
 router.patch("/:id", async (req, res) => {
   const query = { _id: new ObjectId(req.params.id) };
   const updates =  {
@@ -44,17 +59,17 @@ router.patch("/:id", async (req, res) => {
     }
   };
 
-  let collection = await db.collection("records");
+  let collection = await db.collection("posts");
   let result = await collection.updateOne(query, updates);
 
   res.send(result).status(200);
 });
 
-// This section will help you delete a record
+// DELETE
 router.delete("/:id", async (req, res) => {
   const query = { _id: new ObjectId(req.params.id) };
 
-  const collection = db.collection("records");
+  const collection = db.collection("posts");
   let result = await collection.deleteOne(query);
 
   res.send(result).status(200);
