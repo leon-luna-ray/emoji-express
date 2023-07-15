119dc8bd4fb889a0b9913f5c96aa58c577d0d7c7
Ray Luna <leon.luna.ray@gmail.com>
2023-07-15 13:09:00 -0700

    refactor to pull and save to db

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,24 @@
-// axios is the method by which requests to the back end are made
 import axios from 'axios';
 
-const url = 'http://localhost:3001/posts';
+const baseURL = import.meta.env.VITE_BASE_API_URL;
 
-// fetch posts from the backend api post route via axios
-export const fetchPosts = () => axios.get(url);
-// send new post to the back end using the createPost action
-export const createPost = newPost => axios.post(url, newPost);
+export const fetchPosts = async () => {
+  try {
+    const response = await axios.get(`${baseURL}/posts`);
+
+    return response;
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    throw error;
+  }
+};
+
+export const createPost = async (newPost) => {
+  try {
+    const response = await axios.post(`${baseURL}/posts`, newPost);
+    return response;
+  } catch (error) {
+    console.error('Error creating post:', error);
+    throw error;
+  }
+};
