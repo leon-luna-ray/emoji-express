0f4ba0ab9145bb5204004a1dc9cc475ad82d7d1c
Ray Luna <leon.luna.ray@gmail.com>
2023-09-04 09:37:17 -0700

    set up jwt auth routes

--- server/index.mjs
+++ server/index.mjs
@@ -1,17 +1,26 @@
 import express from "express";
+import mongoose from 'mongoose';
 import cors from "cors";
 import "./loadEnvironment.mjs";
 import posts from "./routes/post.mjs";
+import authRoutes from './routes/auth.mjs';
+import protectedRoutes from './routes/protected.mjs';
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.use(cors());
 app.use(express.json());
 
+app.use('/auth', authRoutes);
+app.use('/protected', protectedRoutes);
 app.use("/api/v1/posts", posts);
 
-// start the Express server
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+  console.log(`📡 Server is running on port: ${PORT}`);
 });
