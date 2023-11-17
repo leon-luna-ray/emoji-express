bb13e43bb4bb475b9ab376b6c7ba3949cca02340
Ray Luna <leon.luna.ray@gmail.com>
2023-11-17 11:14:51 -0800

    import fonts

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -4,7 +4,9 @@ import '../styles/views/home.css'
 const HomePage = () => {
     return (
         <div className="container page-center">
-            <h1>Emoji Express</h1>
+            <h1 className='logo-text'>EmojiExpress</h1>
+            <h2>Today I feel</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem esse minima, eveniet assumenda numquam amet nihil corporis blanditiis praesentium eaque laborum odio commodi reprehenderit libero, ipsum alias dolorem vel placeat.</p>
         </div>
     )
 }
