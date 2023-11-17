158095f47299fb9d2c9fede77025b9bfb7ecbd8c
Ray Luna <leon.luna.ray@gmail.com>
2023-11-16 21:58:36 -0800

    test user auth routes

--- routes/auth.mjs
+++ routes/auth.mjs
@@ -9,7 +9,8 @@ router.post('/register', async (req, res) => {
   try {
     const { email, password } = req.body;
     // const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ email, password: hashedPassword });
+    // const user = new User({ email, password: hashedPassword });
+    const user = new User({ email, password: password });
 
     await user.save();
     res.json({ message: 'User registered successfully' });
@@ -23,16 +24,17 @@ router.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-
+    console.log(user)
     if (!user) {
       return res.status(401).json({ error: 'Authentication failed' });
     }
 
+    // TODO FIX BYCRYPT
     // const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    if (!isPasswordValid) {
-      return res.status(401).json({ error: 'Authentication failed' });
-    }
+    // if (!isPasswordValid) {
+    //   return res.status(401).json({ error: 'Authentication failed' });
+    // }
 
     const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
       expiresIn: '1h',
