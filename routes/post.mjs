5fa10eccd8e6500f5781b879ec650a232e76b3e0
Ray Luna <leon.luna.ray@gmail.com>
2023-11-16 22:06:36 -0800

    temp disable unused routes

--- routes/post.mjs
+++ routes/post.mjs
@@ -1,10 +1,9 @@
-// routes/posts.mjs
 import express from 'express';
 import Post from '../models/Post.mjs';
 
 const router = express.Router();
 
-// GET all posts
+// GET
 router.get('/', async (req, res) => {
   try {
     const posts = await Post.find();
@@ -13,19 +12,17 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: 'Could not fetch posts' });
   }
 });
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const post = await Post.findById(req.params.id);
+//     if (!post) return res.status(404).json({ error: 'Post not found' });
+//     res.json(post);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Could not fetch the post' });
+//   }
+// });
 
-// GET a specific post by ID
-router.get('/:id', async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (!post) return res.status(404).json({ error: 'Post not found' });
-    res.json(post);
-  } catch (error) {
-    res.status(500).json({ error: 'Could not fetch the post' });
-  }
-});
-
-// POST a new post
+// POST
 router.post('/', async (req, res) => {
   try {
     const newPost = new Post(req.body);
@@ -37,28 +34,28 @@ router.post('/', async (req, res) => {
   }
 });
 
-// PATCH (update) a post by ID
-router.patch('/:id', async (req, res) => {
-  try {
-    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    if (!post) return res.status(404).json({ error: 'Post not found' });
-    res.json(post);
-  } catch (error) {
-    res.status(500).json({ error: 'Could not update the post' });
-  }
-});
+// PATCH
+// router.patch('/:id', async (req, res) => {
+//   try {
+//     const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+//       new: true,
+//     });
+//     if (!post) return res.status(404).json({ error: 'Post not found' });
+//     res.json(post);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Could not update the post' });
+//   }
+// });
 
-// DELETE a post by ID
-router.delete('/:id', async (req, res) => {
-  try {
-    const post = await Post.findByIdAndDelete(req.params.id);
-    if (!post) return res.status(404).json({ error: 'Post not found' });
-    res.json({ message: 'Post deleted successfully' });
-  } catch (error) {
-    res.status(500).json({ error: 'Could not delete the post' });
-  }
-});
+// DELETE
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     const post = await Post.findByIdAndDelete(req.params.id);
+//     if (!post) return res.status(404).json({ error: 'Post not found' });
+//     res.json({ message: 'Post deleted successfully' });
+//   } catch (error) {
+//     res.status(500).json({ error: 'Could not delete the post' });
+//   }
+// });
 
 export default router;
