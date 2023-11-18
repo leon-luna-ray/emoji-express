9211159792a20b21d29c1d860ef0bc6d2cd8a33b
Ray Luna <leon.luna.ray@gmail.com>
2023-11-17 19:05:58 -0800

    add home page hero

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,16 +17,20 @@ const HomePage = () => {
             const randomIndex = Math.floor(Math.random() * faceEmojis.length);
             setCurrentIndex(randomIndex);
         };
-        const intervalId = setInterval(updateIndex, 1000); // Change interval to 1000 milliseconds (1 second)
+        const intervalId = setInterval(updateIndex, 800);
         return () => clearInterval(intervalId);
     }, []);
 
     return (
-        <div className="h-[500px] flex items-center bg-yellow-400 text-black">
+        <div className="py-[3rem] md:py-[6rem] flex items-center bg-yellow-400 text-black">
             <div className="container flex-col-1 text-center w-[70%] justify-center">
                 <h1 className='py-[1rem]'>Today I feel {faceEmojis[currentIndex]}</h1>
                 <h2>Welcome to Emoji Express!</h2>
                 <p>Embark on a journey of self-discovery and emotional well-being with our unique mood tracking app. 🚀 Whether you're feeling ecstatic, calm, or somewhere in between, Emoji Express lets you effortlessly capture your emotions with just a tap of an emoji. 🎉</p>
+                <div className="flex gap-x-[2rem] justify-center pt-[2rem]">
+                    <div className="btn">Login</div>
+                    <div className="btn">Signup</div>
+                </div>
             </div>
         </div>
     )
