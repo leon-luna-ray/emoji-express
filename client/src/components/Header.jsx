0354bd2f079e6d7e471d523c3e402a07a22b3a3b
Ray Luna <leon.luna.ray@gmail.com>
2025-08-13 22:36:27 -0700

    header styles

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,40 +12,43 @@ const Header = () => {
 
   return (
     <header className='mt-[1rem] w-full'>
-      <div className='container p-[2rem] flex justify-between items-center bg-black'>
-        <div className="flex flex-col text-yellow">
+      <div className='container'>
 
-          <Link to='/' className='logo-text'>
-            EmojiExpress
-          </Link>
-          <p className='text-[0.85rem]'>How are you feeling today?</p>
-        </div>
-        {/* <nav>
+        <div className='p-[2rem] flex justify-between items-center bg-black'>
+          <div className="flex flex-col text-yellow">
+
+            <Link to='/' className='logo-text'>
+              EmojiExpress
+            </Link>
+            <p className='text-[0.85rem]'>How are you feeling today?</p>
+          </div>
+          {/* <nav>
           <ul className='flex gap-x-[1rem] font-[200]'>
-            {isLoggedIn ?
-              <>
-                {user?.email ?
-
-                  <li>
-                    <span className='opacity-50'>{user.email}</span>
-                  </li> : ''
-                }
-                <li>
-                  <Link to='/dashboard' className={isSelected('/dashboard') ? 'underline' : ''}>Dashboard</Link>
-                </li>
-                <li>
-                  <Link onClick={logOut} to='/login' >Logout</Link>
-                </li>
-              </>
-              :
-              <li>
-                <Link to='/login' className={isSelected('/login') ? 'underline' : ''}>
-                  Login
-                </Link>
-              </li>
-            }
+          {isLoggedIn ?
+          <>
+          {user?.email ?
+          
+          <li>
+          <span className='opacity-50'>{user.email}</span>
+          </li> : ''
+          }
+          <li>
+          <Link to='/dashboard' className={isSelected('/dashboard') ? 'underline' : ''}>Dashboard</Link>
+          </li>
+          <li>
+          <Link onClick={logOut} to='/login' >Logout</Link>
+          </li>
+          </>
+          :
+          <li>
+          <Link to='/login' className={isSelected('/login') ? 'underline' : ''}>
+          Login
+          </Link>
+          </li>
+          }
           </ul>
-        </nav> */}
+          </nav> */}
+        </div>
       </div>
     </header>
   )
