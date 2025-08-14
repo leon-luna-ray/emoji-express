41b4fd542ee100df81c4ee20f7f0a06677eeaab9
Ray Luna <leon.luna.ray@gmail.com>
2025-08-13 22:01:38 -0700

    update tailwind, vite

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import svgrPlugin from 'vite-plugin-svgr'
-
+import tailwindcss from '@tailwindcss/vite'
 // see all documentation here https://vitejs.dev/config/
 export default defineConfig({
   // This changes the out put dir from dist to build change as your need
@@ -22,5 +22,6 @@ export default defineConfig({
         // ...svgr options (https://react-svgr.com/docs/options/)
       },
     }),
+    tailwindcss(),
   ],
 })
